d67e2c4c71c10024b0d74586f924f6cc8d3d5e8b
Dave Moore <dmoore@cs.berkeley.edu>
2015-10-07 21:02:19 -0700

    memory limits

--- run_cloud.sh
+++ run_cloud.sh
@@ -4,4 +4,5 @@ source /home/sigvisa/.bash_profile
 source /home/sigvisa/.virtualenvs/sigvisa/bin/activate
 
 cd /home/sigvisa/python/gprf/
+ulimit -Sv 12000000 #set 12gb ram limit so we don't overload other processes
 python $@ >> /home/sigvisa/gprf_log.txt 2>&1
